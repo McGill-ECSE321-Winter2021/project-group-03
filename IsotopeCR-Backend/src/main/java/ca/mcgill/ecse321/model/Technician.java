267389a5faded98a5a2910c2593166eff7842a58
Jiatong-Niu <jiatong.niu@mail.mcgill.ca>
2021-02-20 16:09:38 -0500

    change list in jpa to set.

--- IsotopeCR-Backend/src/main/java/ca/mcgill/ecse321/model/Technician.java
+++ IsotopeCR-Backend/src/main/java/ca/mcgill/ecse321/model/Technician.java
@@ -1,55 +1,41 @@
 package ca.mcgill.ecse321.model;
 
-import java.util.*;
-import java.sql.Time;
-import  javax.persistence.Entity;
 import  java.util.*;
+import  java.sql.Time;
+import  javax.persistence.Entity;
 import  javax.persistence.OneToMany;
 import  javax.persistence.Id;
 import  javax.persistence.CascadeType;
+import  javax.persistence.DiscriminatorValue;
 
 @Entity
+@DiscriminatorValue("Technician")
+
 public class Technician extends Profile
 {
-
- 
-
-
-  //------------------------
-  // CONSTRUCTOR
-  //------------------------
-
-  public Technician(String aFirstName, String aLastName, String aEmail, String aPassword, boolean aIsRegisteredAccount, AutoRepairShop aAutoRepairShop)
-  {
-    super(aFirstName, aLastName, aEmail, aPassword, aIsRegisteredAccount, aAutoRepairShop);
-    services = new ArrayList<Service>();
-    dailyAvailabilities = new ArrayList<DailyAvailabilities>();
-  }
-
   
-  
-  private List<Service> services;
+  private Set<Service> services;
   @OneToMany(cascade= {CascadeType.ALL})
-   public List<Service> getServices()
+   public Set<Service> getServices()
   {
     return this.services;
   }
 
-  public void setService(List<Service> services) {
+  public void setService(Set<Service> services) {
 	  this.services = services;
   }
 
  
   
  
-  private List<DailyAvailabilities> dailyAvailabilities;
+  private Set<DailyAvailabilities> dailyAvailabilities;
   @OneToMany(cascade= {CascadeType.ALL})
-  public List<DailyAvailabilities> getDailyAvailabilities()
+  public Set<DailyAvailabilities> getDailyAvailabilities()
   {
     return this.dailyAvailabilities;
   }
   
-  public void setDailyAvailabilities(List<DailyAvailabilities> dailyAvailabilities) {
+  public void setDailyAvailabilities(Set<DailyAvailabilities> dailyAvailabilities) {
 	  this.dailyAvailabilities=dailyAvailabilities;
   }
   
