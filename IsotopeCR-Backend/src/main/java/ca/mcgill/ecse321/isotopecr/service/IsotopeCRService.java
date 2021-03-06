dee7ff381eb7c081383d473d7bb6dc21470f62d6
Zichen <changzichen2000@gmail.com>
2021-03-06 16:16:00 -0500

    Directories for Sprint2

---
+++ IsotopeCR-Backend/src/main/java/ca/mcgill/ecse321/isotopecr/service/IsotopeCRService.java
@@ -0,0 +1,14 @@
+package ca.mcgill.ecse321.isotopecr.service;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+public class IsotopeCRService {
+
+}
