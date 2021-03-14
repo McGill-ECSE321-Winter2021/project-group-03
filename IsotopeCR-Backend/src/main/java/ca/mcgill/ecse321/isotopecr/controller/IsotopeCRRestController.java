5cd718c070c1e84d61b12e7dc8d8cea61e6152e8
Zichen <changzichen2000@gmail.com>
2021-03-13 21:14:21 -0500

    bookAppointment controller

--- IsotopeCR-Backend/src/main/java/ca/mcgill/ecse321/isotopecr/controller/IsotopeCRRestController.java
+++ IsotopeCR-Backend/src/main/java/ca/mcgill/ecse321/isotopecr/controller/IsotopeCRRestController.java
@@ -3,6 +3,7 @@ package ca.mcgill.ecse321.isotopecr.controller;
 import java.sql.Date;
 import java.sql.Time;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,6 +32,7 @@ import ca.mcgill.ecse321.isotopecr.dao.VehicleRepository;
 import ca.mcgill.ecse321.isotopecr.dto.*;
 import ca.mcgill.ecse321.isotopecr.model.*;
 import ca.mcgill.ecse321.isotopecr.service.IsotopeCRService;
+
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -66,45 +68,84 @@ public class IsotopeCRRestController {
 	AppointmentRepository appointmentRepository;
 	
 	
-	/* ----------------------- Zichen --------------------------*/
+	/**
+	 * @author Zichen
+	 * @return List of ResourceDtos.
+	 */
 	@GetMapping(value = { "/resources", "/resources/" })
 	public List<ResourceDto> getAllResources() {
 		return service.viewAllResources().stream().map(r -> convertToDto(r)).collect(Collectors.toList());
 	}
 	
+	/**
+	 * @author Zichen
+	 * @param type The resourceType
+	 * @param max The maximum availability of one resource
+	 * @return A ResourceDto just created
+	 */
 	@PostMapping(value = { "/events/{type}/{max}", "/events/{type}/{max}" })
-	public ResourceDto createResource(@PathVariable("type") String type, @PathVariable("max") Integer max)
-	throws IllegalArgumentException {
-		// check the input validity first:
-		if (resourceRepository.findResourceByResourceType(type) != null) {
-			throw new IllegalArgumentException("ERROR: the resource type has existed inside the system.");
-		} else if (max < 1) {
-			throw new IllegalArgumentException("ERROR: the resource should at least have one availability.");
+	public ResourceDto createResource(@PathVariable("type") String type, @PathVariable("max") Integer max) 
+			throws RuntimeException{
+		try {
+			Resource resource = service.addResource(type, max);
+			return convertToDto(resource);
+		} catch (IllegalArgumentException e) {
+			throw new RuntimeException(e.getMessage());
 		}
-		Resource resource = service.addResource(type, max);
-		return convertToDto(resource);
 	}
 
+	/**
+	 * @author Zichen
+	 * @return List of InvoiceDtos
+	 */
 	@GetMapping(value = { "/invoice", "/invoice/" })
 	public List<InvoiceDto> getAllInvoices() {
 		return service.viewAllInvoices().stream().map(i -> convertToDto(i)).collect(Collectors.toList());
 	}
 	
+	/**
+	 * @author Zichen
+	 * @return the total income of the system up to now
+	 */
 	@GetMapping(value = { "/incomesummary", "/incomesummary/" })
 	public double getIncomeSummary() {
 		return service.viewIncomeSummary();
 	}
 	
-	@GetMapping(value = { "/availablities/{ID}", "/availabilities/{name}/" })
-	public List<DailyAvailabilityDto> getTechAvailabilities(@PathVariable("name") String profileID) throws IllegalArgumentException {
+	@GetMapping(value = { "/availablities/{ID}", "/availabilities/{ID}/" })
+	public List<DailyAvailabilityDto> getTechAvailabilities(@PathVariable("ID") String profileID) 
+			throws IllegalArgumentException {
 		Technician tech = technicianRepository.findTechnicianByProfileID(profileID);
-		tech.getDailyAvailability();
-//		service.viewAvailability(name)	
-//		Person person = service.createPerson(name);
-//		return convertToDto(person);
+		if (tech == null) {
+			throw new IllegalArgumentException("ERROR: Input profileID not found in system.");
+		}
+		List <DailyAvailabilityDto> dailyAvailabilityDtos = new ArrayList<DailyAvailabilityDto>();
+		for (DailyAvailability dailyavailabilities : tech.getDailyAvailability()) {
+			dailyAvailabilityDtos.add(convertToDto(dailyavailabilities));
+		}
+		
+		return dailyAvailabilityDtos;
 	}
 	
 	
+	/**
+	 * @author Zichen
+	 * @param profileID profileID stored in database
+	 * @return List of DailyAvailabilityDto related to input technician
+	 * @throws IllegalArgumentException
+	 */
+	@GetMapping(value = {"/appointment/{vehicle}/{service}", "/appointment/{vehicle}/{service}"})
+	public AppointmentDto bookAppointment(@PathVariable("customer") String customerEmail, @PathVariable("vehicle") String licensPlate,
+			@PathVariable("technician") String technicianID, @PathVariable("invoice") String invoiceID, @PathVariable("service") String serviceName,
+			@RequestParam Time start, @RequestParam Date date) throws IllegalArgumentException {
+		Vehicle vehicle = vehicleRepository.findVehicleByLicensePlate(licensPlate);
+		Customer customer = customerRepository.findCustomerByVehicle(vehicle);	
+		Invoice invoice = invoiceRepository.findInvoiceByInvoiceID(invoiceID);
+		Service serviceInSystem = serviceRepository.findServiceByServiceName(serviceName);
+		Technician technician = service.getFreeTechnician(start, date);
+		
+		service.bookAppointment(customer, vehicle, technician, invoice, serviceInSystem, start, date);
+	}
 	
 	
 	/* ============================== Helpers ===============================*/
@@ -127,7 +168,7 @@ public class IsotopeCRRestController {
 	
 	private DailyAvailabilityDto convertToDto(DailyAvailability d) {
 		if (d == null) {
-			throw new IllegalArgumentException("There is no such Invoice!");
+			throw new IllegalArgumentException("There is no such dailyAvailability!");
 		}
 		DailyAvailabilityDto dailyAvailabilityDto = new DailyAvailabilityDto(d.getAvailabilityID(), d.getStartTime(), d.getEndTime(), d.getDay());
 		return dailyAvailabilityDto;
