edb8286b29340d412d0d81ea850e76febe0b27da
Victoria Iannotti <iannotti.victoria@gmail.com>
2021-03-30 22:58:58 -0400

    changes in create customer profile frontend

---
+++ IsotopeCR-Frontend/src/components/CreateCustomerProfile.vue
@@ -0,0 +1,88 @@
+<template>
+  <html lang="en">
+    <div id="createAccount">
+      <h3>Create Customer Profile</h3>
+      <div class="container-fluid">
+        <div class="input">
+          <div class="inputbox">
+            <input
+              type="text"
+              class="form-control input-style"
+              v-model="firstName"
+              placeholder="First Name"
+            />
+          </div>
+          <div class="inputbox">
+            <input
+              type="text"
+              class="form-control input-style"
+              v-model="lastName"
+              placeholder="Last Name"
+            />
+          </div>
+          <div class="inputbox">
+            <input
+              type="email"
+              class="form-control input-style"
+              v-model="email"
+              placeholder="Email"
+            />
+          </div>
+          <div class="inputbox">
+            <input
+              type="text"
+              class="form-control input-style"
+              v-model="phoneNumber"
+              placeholder="Phone Number"
+            />
+          </div>
+          <div class="inputbox">
+            <input
+              type="password"
+              class="form-control input-style"
+              v-model="password"
+              placeholder="Password"
+            />
+          </div>
+          <div class="inputbox">
+            <input
+              type="password"
+              class="form-control input-style"
+              v-model="confirmPassword"
+              placeholder="Confirm Password"
+            />
+          </div>
+        </div>
+      </div>
+      <button class="btn btn-danger" @click="createAc">Create Account</button>
+      <p>{{ error }}</p>
+    </div>
+  </html>
+</template>
+
+<script src="./createCustomerProfileScript.js">
+</script>
+
+<style scoped>
+#createAccount {
+  padding-top: 20vh;
+}
+.input {
+  top: 20px;
+  padding: 30px 30px;
+  width: 450px;
+}
+.inputbox {
+  position: relative;
+  text-align: center;
+  align-self: center;
+  padding: 5px 5px;
+}
+.container-fluid {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: auto;
+  white-space: nowrap;
+}
+</style>
--- IsotopeCR-Frontend/src/router/index.js
+++ IsotopeCR-Frontend/src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Home from '@/components/Home'
+import Login from '@/components/Login'
+import CreateCustomerProfile from '@/components/CreateCustomerProfile'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/app',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/',
+      name: 'CreateCustomerProfile',
+      component: CreateCustomerProfile
     }
   ]
 })
