d26c0de488206d816016825de8bc56e9b03065af
Zichen <changzichen2000@gmail.com>
2021-04-03 10:17:03 -0400

    ResponseEntityHandler v_1

--- IsotopeCR-Backend/src/main/java/ca/mcgill/ecse321/isotopecr/controller/RestResponseEntityExceptionHandler.java
+++ IsotopeCR-Backend/src/main/java/ca/mcgill/ecse321/isotopecr/controller/RestResponseEntityExceptionHandler.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
 	@ExceptionHandler(value = { IllegalArgumentException.class, RuntimeException.class })
 	protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
-		String bodyOfResponse = "This should be application specific";
+		String bodyOfResponse = ex.getMessage();
 		return handleExceptionInternal(ex, bodyOfResponse, 
 				new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
 	}
