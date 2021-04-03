7d158c40f0647c97f6458d13378c7639518ae7c1
Yi Wei <59746876+jyiwei@users.noreply.github.com>
2021-04-03 02:10:18 -0400

    quick fix

---
+++ IsotopeCR-Frontend/src/components/Calendar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Fullcalendar/>
+    </div>
+</template>
+
+
+<script>
+import Fullcalendar from '@fullcalendar/vue'
+
+export default {
+    components: {Fullcalendar}
+}
+</script>
+
+<style>
+
+</style>
