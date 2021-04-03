fd15dae3f7f58c5f5587051ae3639642c8daf3d6
Victoria Iannotti <iannotti.victoria@gmail.com>
2021-04-03 00:39:53 -0400

    customer menu

---
+++ IsotopeCR-Frontend/src/components/CustomerMenu.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="input">
+      <b-card title="My profile" class="card">
+        <b-button variant="primary" class="button">
+          View Profile
+        </b-button>
+        <b-button variant="primary" class="button">
+          Update Profile
+        </b-button>
+      </b-card>
+    </div>
+    <div class="input">
+      <b-card title="Appointments" class="card">
+        <b-button variant="primary" class="button">
+          Book Appointment
+        </b-button>
+        <b-button variant="primary" class="button">
+          Cancel Appointment
+        </b-button>
+        <b-button variant="primary" class="button">
+          Upcoming Appointments
+        </b-button>
+        <b-button variant="primary" class="button">
+          Appointment History
+        </b-button>
+      </b-card>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.input {
+  display: flex;
+  justify-content: center;
+}
+.card {
+  margin-top: 20px;
+}
+.button {
+  height: 10rem;
+  width: 10rem;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
