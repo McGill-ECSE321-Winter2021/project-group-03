fd9b1e0e6f42c520abd3e270e91166852f441f9a
Yi Wei <59746876+jyiwei@users.noreply.github.com>
2021-04-03 21:33:27 -0400

    almost done

--- IsotopeCR-Frontend/src/components/CreateResource.vue
+++ IsotopeCR-Frontend/src/components/CreateResource.vue
@@ -1,27 +1,9 @@
 <template>
-<<<<<<< HEAD
 
     <div id="resource" class="container">
-=======
-  <div id="service" class="container"> 
->>>>>>> 11cc278518b8932da41c1d3a37825e60a8447d5e
     <div class="inner_container shadow">
       <br>
       <div class="large_text"><b>Resources</b></div>
-      <hr />
-      <table id="services" class="table">
-        <th>Resource Type</th>
-        <th>Maximum Available  </th>
-        <tr v-for="resource in resources">
-          <td>{{ resource.resourceType }}</td>
-          <td>{{ resource.maxAvailable }}</td>
-        </tr>
-      </table>
-
-<<<<<<< HEAD
-    <div class="large_text"> <b>Resources</b> </div>
-
-    <hr>
    
     <table id="resources" class="table">
       <th>Resource Type</th>
@@ -52,43 +34,13 @@
               v-model="maxAvailable"
               placeholder="Maximum Available"
             />
-=======
-      <hr />
-      <div id="createResource">
-        <h3>Create Resource</h3>
-        <div class="container-fluid">
-          <div class="input">
-            <div class="inputbox">
-              <input
-                type="text"
-                class="form-control input-style"
-                v-model="resourceType"
-                placeholder="Resource Type"
-              />
-            </div>
-            <div class="inputbox">
-              <input
-                type="number"
-                class="form-control input-style"
-                v-model="maxAvailable"
-                placeholder="Maximum Available (>=1)"
-              />
-            </div>
->>>>>>> 11cc278518b8932da41c1d3a37825e60a8447d5e
           </div>
         </div>
-        <button
-          class="btn btn-danger"
-          @click="createResource(resourceType, maxAvailable)"
-        >
-          Create Resource
-        </button>
-        <p v-if="errorcreateResource" style="color: red">{{ errorcreateResource }}</p>
+
       </div>
-<<<<<<< HEAD
-      <button class="btn btn-danger" @click="createResource(resourceType, maxAvailable)">Create Resource</button>
-      <p v-if="errorResource" style="color: red">{{ errorResource }}</p>
-    </div>
+        <button class="btn btn-danger" @click="createResource(resourceType, maxAvailable)">Create Resource</button>
+        <p v-if="errorcreateResource" style="color: red">{{ errorcreateResource }}</p>
+       </div>
 
         <div id="deleteResource">
       <h3>Delete Resource</h3>
@@ -104,32 +56,6 @@
       <hr>
 
     </div> 
-=======
-<br>
-<br>
-      <div id="deleteResource">
-        <h3>Delete Resource</h3>
-        <div class="container-fluid">
-          <div class="input">
-            <div class="inputbox">
-              <input
-                type="text"
-                class="form-control input-style"
-                v-model="deletedResourceType"
-                placeholder="Resource Type"
-              />
-            </div>
-          </div>
-        </div>
-        <button
-          class="btn btn-danger"
-          @click="deleteResource(deletedResourceType)"
-        >
-          Delete Resource
-        </button>
-        <p v-if="errorResource" style="color: red">{{ errorResource }}</p>
-      </div>
->>>>>>> 11cc278518b8932da41c1d3a37825e60a8447d5e
 
       <hr />
     </div>
