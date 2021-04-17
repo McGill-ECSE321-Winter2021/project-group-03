5af5fd4c90ecd4e0826a979b116dcdd4de211c09
Zichen <changzichen2000@gmail.com>
2021-04-16 20:35:35 -0400

    Clean up xml files

--- IsotopeCR-Android/app/src/main/java/ca/mcgill/ecse321/isotopecr/MainActivity.java
+++ IsotopeCR-Android/app/src/main/java/ca/mcgill/ecse321/isotopecr/MainActivity.java
@@ -90,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_login, R.id.nav_slideshow, R.id.nav_bookappointment, R.id.nav_viewappointment)
+                R.id.nav_home, R.id.nav_login, R.id.nav_bookappointment, R.id.nav_viewappointment)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
